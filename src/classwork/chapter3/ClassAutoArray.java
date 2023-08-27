5d6abb358fc33726ddf5450405bcd78c061fe536
Karo Grigorian <karogrigorian8@gmail.com>
2023-08-27 20:54:41 +0400

    book examples

---
+++ src/classwork/chapter3/ClassAutoArray.java
@@ -0,0 +1,9 @@
+package classwork.chapter3;
+
+public class ClassAutoArray {
+    public static void main(String[] args) {
+        int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31 , 30, 31, 30, 31 };
+
+        System.out.println("In April " + month_days[3] + " days");
+    }
+}
---
+++ src/classwork/chapter3/ClassAverage.java
@@ -0,0 +1,12 @@
+package classwork.chapter3;
+
+public class ClassAverage {
+    public static void main(String[] args) {
+        double nums[] = {10.1, 11.2, 12.3, 13.4, 14.5};
+        double result = 0;
+        for(int i = 0; i < 5; i++)
+            result = result + nums[i];
+
+        System.out.println("The mean value is " + result / 5);
+    }
+}
