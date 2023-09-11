e111c118b015b7bb51842a4e5a6cf78b0caee993
Karo Grigorian <karogrigorian8@gmail.com>
2023-09-11 23:14:29 +0400

    homework(sort)

---
+++ src/homeworks/Sort.java
@@ -0,0 +1,25 @@
+package homeworks;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] array = {4, 7, 1, 3, 9, 0, 2};
+        int t;
+        boolean sorted = false;
+
+        while (!sorted) {
+            sorted = true;
+
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] > array[i + 1]) {
+                    t = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = t;
+                    sorted = false;
+                }
+            }
+        }
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
